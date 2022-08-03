bc92f2d832ad759d4dab9ad15299618dc5ebe14e
Kayziel Contreras <kzielcon@gmail.com>
2022-08-03 19:59:55 +0800

    js loops

---
+++ Javascript/JS Loops/chickenfarm.js
@@ -0,0 +1,12 @@
+function nbYear(p0, percent, aug, p) {
+    years = 0
+    while(p0 < p)
+    {
+      p0 += parseInt(p0 * (percent/100)) + aug
+        
+      years++
+    }
+    
+    return years
+  
+  }
